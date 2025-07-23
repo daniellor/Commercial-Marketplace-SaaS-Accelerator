3ac3f61e26e09955847c035834e84222c7b231c8
Daniel Romanowski <daniel@dotlink.pl>
2025-07-23 21:05:50 +0200

    Update OpenID Connect options in Program.cs
    Comment out CallbackPath and add SkipUnrecognizedRequests.

--- src/CustomerSite/Program.cs
+++ src/CustomerSite/Program.cs
@@ -88,7 +88,8 @@ public class Program
                 options.Authority = $"{config.AdAuthenticationEndPoint}/common/v2.0";
                 options.ClientId = config.MTClientId;
                 options.ResponseType = OpenIdConnectResponseType.IdToken;
-                options.CallbackPath = "/Home/Index";
+                //options.CallbackPath = "/Home/Index";
+                options.SkipUnrecognizedRequests = true;
                 options.SignedOutRedirectUri = config.SignedOutRedirectUri;
                 options.TokenValidationParameters.NameClaimType = ClaimConstants.CLAIM_SHORT_NAME;
                 options.TokenValidationParameters.ValidateIssuer = false;
