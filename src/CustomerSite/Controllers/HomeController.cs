a33863f58858b45c18422c4e45099ae91045a9cd
Daniel Romanowski <daniel@dotlink.pl>
2025-07-25 10:21:08 +0200

    Update RedirectUri to absolute URL for authentication
    Changed the `RedirectUri` in `AuthenticationProperties` from a relative URL to an absolute URL. This modification directs users to a specific external location after authentication, enhancing the navigation flow.

--- src/CustomerSite/Controllers/HomeController.cs
+++ src/CustomerSite/Controllers/HomeController.cs
@@ -298,7 +298,7 @@ public class HomeController : BaseController
                     return this.Challenge(
                         new AuthenticationProperties
                         {
-                            RedirectUri = "/?token=" + token,
+                            RedirectUri = "https://order-testing.hyperionsystem.app/Home/Index?token=" + token,
                         }, OpenIdConnectDefaults.AuthenticationScheme);
                 }
                 else
